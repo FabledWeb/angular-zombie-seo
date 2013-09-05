b92a8c413b970d0a4d27df0f39532d2a6226de33
Eddie Canales <eddiecanales@gmail.com>
2013-09-05 13:55:28 -0400

    actually use the port that you say in your npm start command

--- service.js
+++ service.js
@@ -2,5 +2,5 @@ var http = require('http')
   , app = require('./app');
 
 exports.start = function () {
-	return http.createServer(app).listen(3000, function () { console.log('running on port ' + app.get('port')) });
+	return http.createServer(app).listen(app.get('port'), function () { console.log('running on port ' + app.get('port')) });
 };
